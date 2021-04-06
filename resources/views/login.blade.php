1eed18a8f813cb584921cc120de4328df8d05cf4
Zeshi97 <zeshishah97@gmail.com>
2021-04-06 11:53:31 -0700

    login page is add

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,16 +3,17 @@
 <div class="container custom-login">
   <div class="row">
     <div class="col-sm-4 col-sm-offset-4">
-      <form>
+      <form action="login" method="POST">
+        @csrf
         <div class="form-group">
           <label for="formGroupExampleInput">Example label</label>
-          <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Example input">
+          <input type="email" name="email" class="form-control" id="formGroupExampleInput" placeholder="Enter your email">
         </div>
         <div class="form-group">
           <label for="formGroupExampleInput2">Another label</label>
-          <input type="text" class="form-control" id="formGroupExampleInput2" placeholder="Another input">
+          <input type="password" name="password" class="form-control" id="formGroupExampleInput2" placeholder="enter your password">
         </div>
-        <button class="btn btn-primary">Click here</button>
+        <button type="submit" class="btn btn-primary">login</button>
       </form>
     </div>
   </div>
